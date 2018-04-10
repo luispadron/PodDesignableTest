8fc6e16fb05bfc516bdceef8519a1dda6de06fe6
Luis Padron <heyluispadron@gmail.com>
2018-04-10 11:03:35 -0400

    Add pod

---
+++ Pods/Target Support Files/Pods-PodDesignableTestTests/Pods-PodDesignableTestTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodDesignableTestTests : NSObject
+@end
+@implementation PodsDummy_Pods_PodDesignableTestTests
+@end
---
+++ README.md
@@ -0,0 +1,1 @@
+Test for cocoapods issue [#7606](https://github.com/CocoaPods/CocoaPods/issues/7606)
